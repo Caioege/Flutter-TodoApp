028032857572a3e995eac1813cba504a54a24f1e
Caioege <caiohenrick231@gmail.com>
2020-07-08 10:49:41 -0300

    finalização

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'models/item.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Todo App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.amber,
+        primarySwatch: Colors.lightBlue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: HomePage(),
@@ -25,9 +28,9 @@ class HomePage extends StatefulWidget {
 
   HomePage() {
     items = [];
-    items.add(Item(title: "Item 1", done: false));
-    items.add(Item(title: "Item 2", done: true));
-    items.add(Item(title: "Item 3", done: true));
+    // items.add(Item(title: "Item 1", done: false));
+    // items.add(Item(title: "Item 2", done: true));
+    // items.add(Item(title: "Item 3", done: true));
   }
 
   @override
@@ -46,11 +49,35 @@ class _HomePageState extends State<HomePage> {
         done: false,
       ));
       newTaskCtrl.text = "";
+      save();
     });
   }
 
   void remove(int index) {
     widget.items.removeAt(index);
+    save();
+  }
+
+  Future load() async {
+    var prefs = await SharedPreferences.getInstance();
+    var data = prefs.getString('data');
+
+    if (data != null) {
+      Iterable decoded = jsonDecode(data);
+      List<Item> result = decoded.map((x) => Item.fromJson(x)).toList();
+      setState(() {
+        widget.items = result;
+      });
+    }
+  }
+
+  save() async {
+    var prefs = await SharedPreferences.getInstance();
+    await prefs.setString('data', jsonEncode(widget.items));
+  }
+
+  _HomePageState() {
+    load();
   }
 
   @override
@@ -76,13 +103,13 @@ class _HomePageState extends State<HomePage> {
         itemCount: widget.items.length,
         itemBuilder: (BuildContext ctxt, int index) {
           final item = widget.items[index];
-
           return Dismissible(
             child: CheckboxListTile(
               title: Text(item.title ?? ""),
               onChanged: (bool value) {
                 setState(() {
                   item.done = value;
+                  save();
                 });
               },
               value: item.done,
@@ -100,7 +127,7 @@ class _HomePageState extends State<HomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: add,
         child: Icon(Icons.add),
-        backgroundColor: Colors.amber,
+        backgroundColor: Colors.lightBlue,
       ),
     );
   }
